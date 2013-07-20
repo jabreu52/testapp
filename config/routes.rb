a126dab471c0d695a97cae7adfb3cc45103a265c
Jamil Abreu <abreu.jamil@gmail.com>
2013-07-19 22:32:43 -0400

    users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Testapp::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
